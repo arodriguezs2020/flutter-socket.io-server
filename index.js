30200c8a38b0c1943e1ce94135945beb3b7a16ee
arodriguezs <dam16.2020@gmail.com>
2022-09-22 09:38:19 +0200

    Cambios sobre bandas terminado

--- index.js
+++ index.js
@@ -10,16 +10,15 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-
+const {PORT} = process.env
 
 // Path publico
 const publicPath = path.resolve(__dirname, 'public')
-
 app.use(express.static(publicPath));
 
 
-server.listen(process.env.PORT, (err) => {
+server.listen(PORT, (err) => {
     if(err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto`, 3000);
+    console.log(`Servidor corriendo en puerto`, PORT);
 });
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,22 @@
 
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metalica'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
 
-    client.on('disconnect', () => { 
+    client.emit('active-bands', bands.getBands());
+
+    client.on('disconnect', () => {
         console.log('Cliente desconectado');
      });
     
@@ -14,4 +25,26 @@ io.on('connection', client => {
 
         io.emit('mensaje', {admin: 'Nuevo mensaje'})
      } );
+
+     client.on('emitir', (payload) => {
+      console.log(payload);
+      io.emit('nuevo', payload);
+     });
+
+     client.on('vote-band', (payload) => {
+         bands.voteBand(payload.id);
+         io.emit('active-bands', bands.getBands());
+     });
+
+     client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());  
+     });
+
+      client.on('delete-band', (payload) => {
+
+         bands.deleteBand(payload.id);
+         console.log(bands.getBands());
+         io.emit('active-bands', bands.getBands());  
+         });
   });
